87c0e46b0ba609efe40aef66cc7d419dd29be40b
Jinyeong <jinyeongpark1205@gamil.com>
2025-03-14 21:50:24 -0700

    feat: add menu bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,29 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+const menuItems = [
+  { title: "Home", path: "/" },
+  // { title: "Projects", path: "/projects" },
+  // { title: "Blogs", path: "/blogs" },
+  { title: "About", path: "/about" },
+  { title: "Contact", path: "/contact" },
+];
 export default function Home() {
   return (
-    <div className="p-5 flex flex-col gap-5">
-      <h1 className=" text-green-500 font-bold">Homepage</h1>
-      <Button className="w-max">Button</Button>
-      <Input className="w-max" placeholder="type here.."></Input>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex justify-between items-center bg-gray-100 px-20 py-5">
+        <h1 className="font-bold text-2xl text-primary">
+          Port<b className="text-[#FE4E59]">folio</b>
+        </h1>
+        <div className="flex justify-end gap-5 items-center">
+          {menuItems.map((item) => (
+            <span key={item.title} className="px-4">
+              {item.title}
+            </span>
+          ))}
+        </div>
+        <Button className="w-max">Sign In</Button>
+      </div>
     </div>
   );
 }
