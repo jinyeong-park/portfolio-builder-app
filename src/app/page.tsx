fa7e9c13385cfa39c619d7426f2065a38135daf6
Jinyeong <jinyeongpark1205@gamil.com>
2025-03-14 22:04:12 -0700

    working on body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <div className="flex justify-between items-center bg-gray-100 px-20 py-5">
         <h1 className="font-bold text-2xl text-primary">
-          Port<b className="text-[#FE4E59]">folio</b>
+          My<b className="#0a0a0a">Folio</b>
         </h1>
         <div className="flex justify-end gap-5 items-center">
           {menuItems.map((item) => (
@@ -24,6 +24,19 @@ export default function Home() {
           ))}
         </div>
         <Button className="w-max">Sign In</Button>
+      </div>
+      <div>
+        <div className="grid grid-cols-2 gap-10 mt-20 h=[70vh] px-20">
+          <div className="text-4xl font-bold text-primary">
+            <h1>MyFolio - Your Personalized Portfolio Builder</h1>
+            <p>
+              MyFolio is a platform that lets you create a personalized
+              portfolio in just minutes. It’s easy to use and packed with
+              features. You can quickly add your projects, skills, and
+              experiences to showcase your work.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
